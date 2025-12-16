ec506b2358d03cde8df1214d8bd7c96c4bd187e1
Suxrob Sagdullayev <suxrobsagdullayev5@gmail.com>
2025-12-16 23:49:16 +0500

    OOP Class/Objects

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+class Car {
+    // property, attribute
+    public $model;
+    public $year;
+
+    // method
+    public function drive(){
+        return 'Driving...';
+    }
+
+    public function getFuel(){
+        return 'Fueling...';
+    }
+
+    public function setModel($model){
+        $this-> model = $model;
+    }
+}
+
+$bmw = new Car();
+
+$bmw->setModel('BMW-M5 Competetion');
+echo $bmw->model;
+
